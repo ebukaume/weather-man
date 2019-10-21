069b3cd68e505eeb85eabc21d13e58b6f872a6b3
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-10-21 10:49:33 +0100

    save API key to environment

--- src/services/weather_api.js
+++ src/services/weather_api.js
@@ -1,10 +1,10 @@
 import fetchAPI from './fetch_api';
 import { kelvinToFahrenheit, kelvinToCelcius, formatDatetime } from '../util';
-import keys from '../../.keys.config';
+// import keys from '../../.keys.config';
 
 const weatherAPI = (() => {
   const BASE_ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather';
-  const API_KEY = keys.WEATHER_API_KEY;
+  const API_KEY = process.env.WEATHER_API_KEY;
 
   const parseWeatherData = (data) => {
     const { country } = data.sys;
